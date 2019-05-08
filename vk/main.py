a47167a5a0c395049e7a844d59bd946cd16a3171
bularond <bularond@gmail.com>
2019-05-08 20:59:22 +0500

    Update main.py I dont remembar, what i change. I hope it work

--- vk/main.py
+++ vk/main.py
@@ -13,22 +13,20 @@ longpoll = VkLongPoll(vk)
 print("Server started")
 
 for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
-        if event.to_me:
-            message = event.text
-            bot = VkBot(event.message_id, event.user_id, hw)
-            
-            print("NEW MESSAGE")
-            print(f"By : {event.user_id}")
-            print(f"Text: {message}")
-            
-            try:
-                answers = bot.new_message(message)
-            except BaseException as err:
-                answers = [{'messege': 'Ошибка',
-                        'user_id': event.user_id,
-                        'random_id': randint(0, 2 ** 32 - 1)}]
-                print(err)
-            for answer in answers:
-                vk.method('messages.send', answer)
-                print('Answer', answer)
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+        message = event.text
+        bot = VkBot(event.message_id, event.user_id, hw)
+        
+        print("NEW MESSAGE")
+        print(f"By : {event.user_id}")
+        print(f"Text: {message}")
+        
+        try:
+            answers = bot.new_message(message)
+        except BaseException:
+            answers = [{'messege': 'Ошибка',
+                    'user_id': event.user_id,
+                    'random_id': randint(0, 2 ** 32 - 1)}]
+        for answer in answers:
+            vk.method('messages.send', answer)
+            print('Answer', answer)
